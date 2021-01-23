c7782d43556a1621bed742cf19fba699601af0fa
Marvin Fretter <marvin.fretter@gmail.com>
2021-01-23 15:22:36 +0100

    corejs added

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Transpiler for older browsers
+import 'core-js/stable';
+// optional but required for transforming generator fns.
+import 'regenerator-runtime/runtime'; 
+
 createApp(App).use(router).mount('#app')
